c41864e30b9431cee8a49d2cbab0326bfafeab69
Tony <tonyrkovar@gmail.com>
2019-10-04 12:37:16 -0500

    got a click event test on useDarkMode running and passing with sanity check

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Display from './components/Display';
+import { useDarkMode } from './hooks/useDarkMode'
+import { render, fireEvent } from '@testing-library/dom';
+import { tsExternalModuleReference } from '@babel/types';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('fires properly', () => {
+  // const { getByText } = render(<Display />)
+  fireEvent.click[useDarkMode]
+})
